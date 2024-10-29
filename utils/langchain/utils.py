baef2d516c49c8868b3e02350c3225a12c32b078
SuperDryHann <han@askhomer.ai>
2024-10-29 21:42:55 +0000

    message history class corrected

--- utils/langchain/utils.py
+++ utils/langchain/utils.py
@@ -28,17 +28,22 @@ class OrmChatMessageHistory(BaseChatMessageHistory):
         @sync_to_async
         def get_chat_history(user_uuid: str):
             chat_history, created=ChatHistory.objects.get_or_create(user_uuid=user_uuid)
-            return messages_from_dict(chat_history.messages)
+            messages = chat_history.messages or []
+            return messages_from_dict(messages) # When it is initially created, it will be None. So pass empty list.
         
         return await get_chat_history(self.user_uuid)
     
     def clear(self):
         pass
 
-    async def aadd_messages(self, messages: list[BaseMessage]):
+    async def aadd_messages(self, messages: list[dict]):
         @sync_to_async
-        def append_messages(user_uuid: str, messages: list[BaseMessage]):
+        def append_messages(user_uuid: str, messages: list[dict]):
             chat_history, created=ChatHistory.objects.get_or_create(user_uuid=user_uuid)
+
+            if chat_history.messages is None: # Handle initial None case
+                chat_history.messages = []
+
             chat_history.messages.extend(messages)
             chat_history.save()
         await append_messages(self.user_uuid, messages)
